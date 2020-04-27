c3ae8c1c5e1e2162ede4d01f8ed79f06ab63b98a
dnguye2 <dnguyen1093@gmail.com>
2020-04-27 16:33:07 -0700

    added colors that are randomized to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,11 @@ import "./Card.css";
 import emojis from "emoji-dictionary";
 
 const Card = ({ emoji, id, text, deleteCardCallback }) => {
+  // source to randomize a set of colors: https://stackoverflow.com/questions/27799125/random-color-on-different-divs/27799274
+  let postItNoteColors = ["card__color-yellow", "card__color-red", "card__color-green", "card__color-blue", "card__color-pink"]
+
   return (
-    <div className="card card__content">
+    <div className={"card card__content " + postItNoteColors[Math.floor(Math.random() * postItNoteColors.length)]}>
       <p className="card__content-text">{text}</p>
       <span className="card__content-emoji">
         {emojis.getUnicode(`${emoji}`)}
